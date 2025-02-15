e204b7a0607a3f4b2d476a11dbca77185fa9886d
Sadık YILDIRIM <sadik_yildirim@outlook.com>
2025-02-16 00:43:46 +0300

    Batch Renamer

---
+++ BatchRenamer/BatchRenamer.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace BatchRenamer
+{
+    partial class BatchRenamer
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BatchRenamer));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(334, 160);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 282);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(116, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "With random letters";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 319);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(113, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Sequential number";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 354);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(66, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Found in";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(334, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(148, 301);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "letter name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(148, 351);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Drag and drop files or select files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(334, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 203);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(334, 56);
+            this.listBox2.TabIndex = 11;
+            // 
+            // BatchRenamer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BatchRenamer";
+            this.Text = "BatchRenamer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ BatchRenamer/BatchRenamer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatchRenamer
+{
+    public partial class BatchRenamer : Form
+    {
+        public BatchRenamer()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files) listBox1.Items.Add(file);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "start from";
+            textBox1.Enabled = false;
+            numericUpDown1.Enabled = true;
+            numericUpDown1.Value = 1;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "letter name";
+            textBox1.Enabled = false;
+            numericUpDown1.Enabled = true;
+            numericUpDown1.Value = 5;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            numericUpDown1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.ShowDialog(this);
+            if (openFileDialog.FileName != "")
+            { foreach (string file in openFileDialog.FileNames) listBox1.Items.Add(file); }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            listBox1.Items.Clear();
+        }
+        public string RandomName(int wordLenght)
+        {
+            Random rnd = new Random();
+            string word = "";
+            string[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            for (int i = 0; i < wordLenght; i++)
+            {
+                word += letters[rnd.Next(letters.Length)];
+            }
+            return word;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.Items.Count > 0)
+                {
+                    progressBar1.Maximum = listBox1.Items.Count;
+                    if (radioButton1.Checked)
+                    {
+                        foreach (string file in listBox1.Items)
+                        {
+                            FileInfo fileInfo = new FileInfo(file);
+                            int numericalValue = decimal.ToInt32(numericUpDown1.Value);
+                            string word = "";
+                            if (numericalValue > 0)
+                            {
+                                word = RandomName(numericalValue);
+                                while (File.Exists(fileInfo.DirectoryName + "/" + word + fileInfo.Extension))
+                                { word = RandomName(numericalValue); }
+                            }
+
+
+                            string newName = fileInfo.DirectoryName + "/" + word + fileInfo.Extension;
+                            System.IO.File.Move(file, newName);
+                            listBox2.Items.Add(newName);
+                            listBox2.TopIndex = listBox2.Items.Count - 1;
+                            Application.DoEvents();
+                            listBox2.Refresh();
+
+                            progressBar1.Value++;
+                        }
+                        progressBar1.Value = 0;
+                        MessageBox.Show("Success", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        listBox1.Items.Clear();
+                        listBox1.Items.AddRange(listBox2.Items.Cast<object>().ToArray());
+                        listBox2.Items.Clear();
+
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        foreach (string file in listBox1.Items)
+                        {
+                            FileInfo fileInfo = new FileInfo(file);
+                            int numericalValue = decimal.ToInt32(numericUpDown1.Value);
+                            while (File.Exists(fileInfo.DirectoryName + "/" + numericalValue + fileInfo.Extension))
+                                numericalValue++;
+
+                            string newName = fileInfo.DirectoryName + "/" + numericalValue++ + fileInfo.Extension;
+                            System.IO.File.Move(file, newName);
+                            listBox2.Items.Add(newName);
+                            listBox2.TopIndex = listBox2.Items.Count - 1;
+                            Application.DoEvents();
+                            listBox2.Refresh();
+
+                            progressBar1.Value++;
+                        }
+                        progressBar1.Value = 0;
+                        MessageBox.Show("Success", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        listBox1.Items.Clear();
+                        listBox1.Items.AddRange(listBox2.Items.Cast<object>().ToArray());
+                        listBox2.Items.Clear();
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch(Exception error ){
+                MessageBox.Show(error.ToString(),"ERROR",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
